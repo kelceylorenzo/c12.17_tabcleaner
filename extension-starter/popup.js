07aba61cac4702febe22566d97cc156df1e367d8
Nick Quan <nickkquan@gmail.com>
2018-02-03 09:49:16 -0800

    completed grabbing ID of tab and displaying on extension DOM

--- extension-starter/popup.js
+++ extension-starter/popup.js
@@ -1,1 +1,14 @@
-console.log("Test Environment Functioning")
+console.log("Test Environment Functioning");
+
+chrome.tabs.query({}, function(tabs) {
+	for (var tabLinkIndex = 0; tabLinkIndex < tabs.length; tabLinkIndex++) {
+		var paragraph = document.createElement("p");
+		var urlId = document.createTextNode(tabs[tabLinkIndex].id);
+		paragraph.appendChild(urlId);
+		document.body.appendChild(paragraph);
+	}
+	console.log(tabs);
+});
+
+// // console command for forcing re-loading debugger 
+// location.reload(true)
