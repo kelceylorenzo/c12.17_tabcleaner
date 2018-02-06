256e0b57318815790504b640d939cdc2f2d30271
Andrea <andrea.wayte@gmail.com>
2018-02-06 11:17:23 -0800

    keep track of elapsed time

--- tab-project/popup.js
+++ tab-project/popup.js
@@ -1,15 +1,16 @@
-chrome.runtime.sendMessage(
+document.addEventListener('DOMContentLoaded', function() {	
+  chrome.runtime.sendMessage(
     "popup",
     function (response) {
       var index = 0 
       for(var item in response){
         var tabInfo = response[item];
         var tabEl = document.createElement('LI');
-        var span = document.createElement('span');
-        var addSpanText = document.createTextNode(" | DELETE");
+        var span = document.createElement('i');
+        span.className = "far fa-trash-alt";
+        // var addSpanText = document.createTextNode(" | DELETE");
         var addText = document.createTextNode(tabInfo.title);
         tabEl.className = "id" + tabInfo.id;
-        span.append(addSpanText);
         tabEl.append(addText);
         span.addEventListener('click', clickEvent.bind(this, tabInfo.id));
         tabEl.addEventListener('click', highlightTab.bind(this, index))
@@ -18,7 +19,9 @@ chrome.runtime.sendMessage(
         index++
       }
     }
-);
+  );
+
+});
 
 function clickEvent(id, event) {
   chrome.tabs.remove(id);
