6f3ada5d267ada9e17dd2033a705c140f0ae41ef
Andrea <andrea.wayte@gmail.com>
2018-02-07 12:35:48 -0800

    include jsDocs

--- tab-project/popup.js
+++ tab-project/popup.js
@@ -1,27 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {	
-  chrome.runtime.sendMessage(
-    "popup",
-    function (response) {
-      var index = 0 
-      for(var item in response){
-        var tabInfo = response[item];
-        var tabEl = document.createElement('LI');
-        var span = document.createElement('i');
-        span.className = "far fa-trash-alt";
-        // var addSpanText = document.createTextNode(" | DELETE");
-        var addText = document.createTextNode(tabInfo.title);
-        tabEl.className = "id" + tabInfo.id;
-        tabEl.append(addText);
-        span.addEventListener('click', clickEvent.bind(this, tabInfo.id));
-        tabEl.addEventListener('click', highlightTab.bind(this, index))
-        tabEl.appendChild(span);
-        document.getElementById('tag-titles').appendChild(tabEl);
-        index++
-      }
+chrome.runtime.sendMessage(
+  "popup",
+  function (response) {
+    for(var item in response){
+      var tabInfo = response[item];
+      createDomElement(tabInfo, tabInfo.index); 
     }
-  );
+  }
+);
 
-});
+
+function createDomElement(tabObject, index){
+  if(!tabObject.title){
+    return; 
+  }
+  var tabEl = document.createElement('LI');
+  var span = document.createElement('i');
+  span.className = "far fa-trash-alt";
+  var addText = document.createTextNode(tabObject.title);
+  tabEl.className = "id" + tabObject.id + " " + tabObject.color;
+  tabEl.append(addText);
+  span.addEventListener('click', clickEvent.bind(this, tabObject.id));
+  tabEl.addEventListener('click', highlightTab.bind(this, index))
+  tabEl.appendChild(span);
+  document.getElementById('tag-titles').appendChild(tabEl);
+}
 
 function clickEvent(id, event) {
   chrome.tabs.remove(id);
