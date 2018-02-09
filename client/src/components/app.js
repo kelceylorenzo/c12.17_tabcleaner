9eace06a763053036c74899f3fd94935b0e919e2
Nick Quan <nickkquan@gmail.com>
2018-02-09 12:45:17 -0800

    stats-page initial setup completed

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import React from "react";
+import StatsPage from "./statsPage";
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
-    </div>
+	<div>
+		<div className="app">
+			<StatsPage />
+		</div>
+	</div>
 );
 
 export default App;
