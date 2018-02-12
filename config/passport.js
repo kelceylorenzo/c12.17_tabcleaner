587707c99a2685d423cd25db5f0f1dfbfff8f57b
James Kirsch <jameskirschaf@gmail.com>
2018-02-12 10:06:12 -0800

    Long story short, I'm an idiot

---
+++ config/passport.js
@@ -0,0 +1,47 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('./keys');
+
+// Load User Model
+const User = mongoose.model('users');
+
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    }, (accessToken, refreshToken, profile, done) => {
+      // console.log(accessToken);
+      // console.log(profile);
+
+      const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+      
+      const newUser ={
+        googleID: profile.id,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        email: profile.emails[0].value,
+        image: image
+      }
+
+      // Check for existing user
+      User.findOne({
+        googleID: profile.id
+      }).then(user =>{
+        if(user){
+          // Return User
+          return done(null, user);
+        } else {
+          // Create User
+          new User(newUser)
+            .save()
+            .then(user=> done(null, user));
+        }
+      })
+
+    })
+  )
+};
+
