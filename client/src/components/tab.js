3eadb79eb489bf7cff1f2a984bbefec5b4262c10
m13kelore <kelceylorenzo@gmail.com>
2018-02-19 12:39:05 -0800

    added hover effects and check marks to denote tab selection on dashboard

--- client/src/components/tab.js
+++ client/src/components/tab.js
@@ -2,25 +2,37 @@ import React from 'react';
 
 export default (props) => {
 	let style = {
-		backgroundColor: '',
-		color: ''
+		display: ''
 	};
 
 	if (props.item.selected) {
-		style.backgroundColor = 'green';
-		style.color = 'white';
+		style.display = 'flex';
 	}
 
 	return (
-		<div className="tab" onClick={props.select} style={style}>
-			<div className="tab-title">{props.item.title}</div>
-			<div className="tab-screenshot" />
-			<div className="tab-utilities-container">
-				<div className="tab-utility open-favicon" onClick={() => props.utilityClick(props.item, 'open')}>
-					<i className="fas fa-external-link-alt" />
+		<div className="tab-container" onClick={props.select}>
+			<div className="select-overlay-container" style={style}>
+				<img className="select-overlay" src="/assets/images/check-mark.png" alt="" />
+			</div>
+
+			<div className="tab">
+				<div className="tab-title">{props.item.title}</div>
+				<div className="tab-screenshot">
+					<img src="/assets/images/e9492f0f82721e4998b1360e409e6fe8affc30bb.png" alt="" />
 				</div>
-				<div className="tab-utility close-favicon" onClick={() => props.utilityClick(props.item, 'close')}>
-					<i className="fas fa-times" />
+				<div className="tab-utilities-container">
+					<div
+						className="tab-utility open-favicon"
+						onClick={() => props.utilityClick(props.item, 'open')}
+					>
+						<i className="fas fa-external-link-alt" />
+					</div>
+					<div
+						className="tab-utility close-favicon"
+						onClick={() => props.utilityClick(props.item, 'close')}
+					>
+						<i className="fas fa-times" />
+					</div>
 				</div>
 			</div>
 		</div>
