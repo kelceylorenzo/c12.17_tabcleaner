72781900c00ccacc9f24e49084d39665de7b2661
Nick Quan <nickkquan@gmail.com>
2018-02-19 12:50:48 -0800

    top ten page styling started

--- client/src/components/top-ten-page.js
+++ client/src/components/top-ten-page.js
@@ -1,71 +1,68 @@
-import React, { Component } from 'react';
-
-import Header from './header';
-import headerData from './header-data.js';
+import React, { Component } from "react";
+import image from "../assets/images/about-us-mainimage.png";
+import Header from "./header";
+import headerData from "./header-data.js";
 
 class TopTenPage extends Component {
 	render() {
 		return (
-			<div>
+			<div className="top-ten-container">
 				<div className="header-container">
 					<Header routes={headerData} />
 				</div>
-				<div className="top-ten-container">
-					<div className="header">
-						<h1>Top 10 Websites You Visit</h1>
+				<div className="website-container">
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot-container">
+							<img className="screenshot" src={image} alt="top top screenshot" />
+							{/* <div className="screenshot-overlay">here is some text</div> */}
+						</div>
+						<div className="time-spent">Time Spent: 120</div>
 					</div>
-
-					<div className="website-container">
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
-						<div className="page">
-							<div className="website-name">Youtube</div>
-							<div className="screenshot" />
-							<div className="time-spent">120</div>
-						</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
+					</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
+					</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
+					</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
+					</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
+					</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
+					</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
+					</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
+					</div>
+					<div className="page">
+						<div className="website-name">Youtube</div>
+						<div className="screenshot" />
+						<div className="time-spent">Time Spent: 120</div>
 					</div>
 				</div>
 			</div>
