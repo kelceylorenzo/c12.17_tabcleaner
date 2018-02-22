26d429b14e175fb7739108eced21b42b5b072f51
James Kirsch <jameskirschaf@gmail.com>
2018-02-22 10:38:40 -0800

    C:/Program Files/Git/tabs/deactivatedTime should now add the current activatedTime to the url table

--- routes/tabs.js
+++ routes/tabs.js
@@ -11,7 +11,7 @@ db.connect((err) => {
     console.log("Connected to remote DB");
 });
 
-function checkIfTableExists (req, res, next){
+function checkIfTableExists(req, res, next) {
     db.query(
         "CREATE TABLE IF NOT EXISTS tabs (" +
         "databaseTabID MEDIUMINT(8) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
@@ -152,7 +152,67 @@ router.put('/:time', ensureAuthenticated, checkIfTableExists, (req, res) => {
     let time = new Date();
     time = time.getTime();
 
-    const { databaseTabID } = req.body;
+    const { databaseTabID, url } = req.body;
+
+    let domain = (url).match(/([a-z0-9|-]+\.)*[a-z0-9|-]+\.[a-z]+/g) || (url).match(/^(chrome:)[//]{2}[a-zA-Z0-0]*/);
+    let domain = domain[0];
+
+    if (req.params.time === 'deactivatedTime') {
+
+        const getActiveTimeQuery = 'SELECT * FROM tabs WHERE databaseTabID = ? LIMIT 1';
+        const getActiveTimeInsert = databaseTabID;
+        const getActiveTimeSQL = mysql.format(getActiveTimeQuery, getActiveTimeInsert);
+
+        db.query(getActiveTimeSQL, (err, results) => {
+
+            const storedActiveTime = results.activatedTime;
+            let newActiveTime = time - storedActiveTime;
+
+            const createUrlTableSQL = "CREATE TABLE IF NOT EXISTS urls (" +
+                "databaseUrlID NOT NULL PRIMARY KEY," +
+                "googleID double NOT NULL," +
+                "url VARCHAR(30) NULL," +
+                "totalActiveTime INT(20) NULL);";
+            
+            db.query(createUrlTableSQL, (err) => {
+                if (err) console.log(err);
+
+                const activeTimeQuery = 'SELECT * FROM urls WHERE googleID=? AND url=? LIMIT 1';
+                const activeTimeInsert = [req.user, domain];
+                const activeTimeSQL = mysql.format(activeTimeQuery, activeTimeInsert);
+
+                db.query(activeTimeSQL, (err, results) => {
+
+                    if (results.length > 0) {
+
+                        let newActiveTime = results.totalActiveTime + newActiveTime;
+
+                        const updateActiveTimeQuery = 'UPDATE urls SET totalActiveTime = ? WHERE databaseUrlID= ? LIMIT 1';
+                        const updateActiveTimeInsert = [newActiveTime, results.databaseUrlID];
+                        const updateActiveTimeSQL = mysql.format(updateActiveTimeQuery, updateActiveTimeInsert);
+
+                        db.query(updateActiveTimeSQL, (err) => {
+                            if (err) console.log(err)
+                            console.log('UPDATED URL in table: domain: ', domain, ', time: ', newActiveTime);
+                        });
+
+                    } else {
+
+                        const insertUrlQuery = 'INSERT INTO urls (googleID, url, totalActiveTime) VALUES (? ? ?))'
+                        const insertUrlInsert = [req.user, domain, newActiveTime];
+                        const insertUrlSQL = mysql.format(insertUrlQuery, insertUrlInsert);
+
+                        db.query(insertUrlSQL, (err, results)=>{
+                            if(err) console.log(err);
+                            console.log(results);
+                            console.log('CREATED URL in table, domain: ', domain, ', time: ', newActiveTime);
+                        });
+
+                    }
+                })
+            })
+        })
+    };
 
     const query = 'Update tabs SET ?? = ? WHERE databaseTabID = ? LIMIT 1';
     const insert = [req.params.time, time, databaseTabID];
@@ -171,30 +231,6 @@ router.put('/:time', ensureAuthenticated, checkIfTableExists, (req, res) => {
         const json_output = JSON.stringify(output);
         res.send(json_output);
     });
-
-
-    // db.query("CREATE TABLE IF NOT EXISTS urls (" +
-    //         "googleID double NOT NULL PRIMARY KEY," +
-    //         "url VARCHAR(30) NULL," +
-    //         "totalActive INT(20) NULLl);", 
-    //         (err) => {
-    //             if (err) console.log(err);
-    //             db.query(insertUser, (err) => {
-    //                 if (err) console.log(err);
-    //                 console.log('User was not in db, but is now');
-    //                 return done(null, newUser);
-    //             });
-    //         }
-    // );
-
-
-    // db.query()
-
-
-
-
-
-
 });
 
 module.exports = router;
