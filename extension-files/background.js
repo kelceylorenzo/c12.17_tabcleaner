3d1562d41ab7d6f510de0fad897d9ebc1df8a405
Andrea <andrea.wayte@gmail.com>
2018-02-22 12:19:07 -0800

    deactivate when add to database for first time

--- extension-files/background.js
+++ extension-files/background.js
@@ -22,6 +22,7 @@ class User {
 			for (var tab in this.tabsSortedByWindow[window]) {
 				var currentTab = this.tabsSortedByWindow[window][tab];
 				createNewTabRequest(currentTab);
+				//deactivate. if no database tab id, return 
 			}
 		}
 	}
