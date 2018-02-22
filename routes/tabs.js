b17249b911ab1ca840f512b3678581c430b7e5e6
James Kirsch <jameskirschaf@gmail.com>
2018-02-21 19:14:07 -0800

    dat bundle tho

--- routes/tabs.js
+++ routes/tabs.js
@@ -12,6 +12,25 @@ db.connect((err) => {
     console.log("Connected to remote DB");
 });
 
+checkIfTableExists: (req, res, next) => {
+    db.query(
+        "CREATE TABLE IF NOT EXISTS tabs (" +
+        "databaseTabID MEDIUMINT(8) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+        "windowID MEDIUMINT(8) NULL ," +
+        "tabTitle VARCHAR(200) NULL," +
+        "activatedTime double NULL," +
+        "deactivatedTime double NULL," +
+        "browserTabIndex int(10) NULL," +
+        "googleID double NULL," +
+        "url VARCHAR(2084) NULL," +
+        "favicon VARCHAR(2084) NULL );",
+        (err) => {
+            if (err) throw err;
+        }
+    );
+    next();
+}
+
 router.get('/', ensureAuthenticated, (req, res) => {
 
     const query = 'SELECT * FROM tabs WHERE googleID=?';
