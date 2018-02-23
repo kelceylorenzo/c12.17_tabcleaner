db72bb0b67fc01fc61cf536f6fcab7104f5ed9bd
James Kirsch <jameskirschaf@gmail.com>
2018-02-22 16:33:34 -0800

    Modified regex to catch localhost

--- routes/tabs.js
+++ routes/tabs.js
@@ -156,7 +156,7 @@ router.put('/:time', ensureAuthenticated, checkIfTableExists, (req, res) => {
 
     if (req.params.time === 'deactivatedTime' && url) {
 
-        let domain = (url).match(/([a-z0-9|-]+\.)*[a-z0-9|-]+\.[a-z]+/g) || (url).match(/^(chrome:)[//]{2}[a-zA-Z0-0]*/);
+        let domain = (url).match(/([a-z0-9|-]+\.)*[a-z0-9|-]+\.[a-z]+/g) || (url).match(/^(chrome:)[//]{2}[a-zA-Z0-0]*/) || (url).match(/^(localhost)/);
         domain = domain[0];
 
         const getActiveTimeQuery = 'SELECT * FROM tabs WHERE databaseTabID = ? LIMIT 1';
