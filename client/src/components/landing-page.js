efd4ea8c997688e5eac19b9fd7b2a1eddccd0328
Nick Quan <nickkquan@gmail.com>
2018-02-22 16:01:07 -0800

    revised package.json file and landing page

--- client/src/components/landing-page.js
+++ client/src/components/landing-page.js
@@ -11,7 +11,7 @@ export default props => {
 			</div>
 
 			<div className="login-button-container">
-				<a className="login-button" href="http://closeyourtabs.com/auth/google/">
+				<a className="login-button" href="/auth/google/">
 					LOG IN
 				</a>
 			</div>
