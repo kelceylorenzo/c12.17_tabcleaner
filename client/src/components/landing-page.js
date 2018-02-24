f0b70bcf61e0a762f3c39c1d89c2e16f8d7978d9
m13kelore <kelceylorenzo@gmail.com>
2018-02-24 15:07:20 -0800

    completed base skeleton for landing page revamp

--- client/src/components/landing-page.js
+++ client/src/components/landing-page.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import image from '../assets/images/app-logo.png';
+import placeholder from '../assets/images/e9492f0f82721e4998b1360e409e6fe8affc30bb.png';
 import axios from 'axios';
 
 class LandingPage extends Component {
@@ -24,16 +25,73 @@ class LandingPage extends Component {
 
 	render() {
 		return (
-			<div className="landing-page-container">
-				<img className="logo" src={image} alt="" />
-				<div className="landing-page-title">
-					<h1>CLOSE YOUR TABS</h1>
+			<div className="home-page-container">
+				<div className="home-showcase-container">
+					<div className="home-showcase">
+						<i className="fas fa-circle" />
+					</div>
+					<div className="home-showcase">
+						<i className="fas fa-circle" />
+					</div>
+					<div className="home-showcase">
+						<i className="fas fa-circle" />
+					</div>
+					<div className="home-showcase">
+						<i className="fas fa-circle" />
+					</div>
+				</div>
+				<div className="landing-page-container">
+					<div className="landing-page-title-container">
+						<img className="logo" src={image} alt="" />{' '}
+						<p className="landing-page-title">CLOSE YOUR TABS</p>
+						<p className="landing-page-subtitle">Keep tabs on your tabs</p>
+					</div>
+
+					<div className="login-button-container">
+						<a className="login-button" href="/auth/google/">
+							LOG IN
+						</a>
+						<a className="login-button" href="/auth/google/">
+							SIGN UP
+						</a>
+					</div>
+				</div>
+				<div className="next-section-button intro">
+					<p>What is Close Your Tabs?</p>
 				</div>
 
-				<div className="login-button-container">
-					<a className="login-button" href="/auth/google/">
-						LOG IN
-					</a>
+				<div className="introduction-container">
+					<div className="introduction-image">
+						<img src={placeholder} alt="" />
+					</div>
+					<div className="introduction-content-container">
+						<p className="landing-page-subtitle">Isn’t it time to close some of those tabs?</p>
+						<p className="landing-page-content">
+							It's simple. Using our Chrome Extension and Dashboard, Close Your Tabs keeps track of
+							the last time you've visited each of your currently open browser tabs and color-codes
+							them accordingly.
+						</p>
+					</div>
+				</div>
+				<div className="next-section-button extension-dashboard">
+					<p>Extension & Dashboard</p>
+				</div>
+
+				<div className="extension-dashboard-container">
+					<div className="landing-page-title-container">
+						<p className="landing-page-subtitle">Two ways to Close Your Tabs</p>
+					</div>
+				</div>
+				<div className="next-section-button getting-started">
+					<p>Getting Started</p>
+				</div>
+				<div className="getting-started-container">
+					<div className="landing-page-title-container">
+						<p className="landing-page-subtitle">Let's Get Started</p>
+					</div>
+				</div>
+				<div className="next-section-button faq-link">
+					<p>Still have question? Visit our FAQ.</p>
 				</div>
 			</div>
 		);
