c118efa976c7d5b454e1b2aaa9bba3fb1e9f5ef9
Andrea <andrea.wayte@gmail.com>
2018-02-27 13:26:55 -0800

    added jsdocs to popup.js

--- extension-files/popup.js
+++ extension-files/popup.js
@@ -91,17 +91,26 @@ function createDomElement(tabObject) {
 	tab.appendChild(trashcanContainer);
 	tab.appendChild(favicon);
 	tab.append(title);
-	trashcan.addEventListener('click', clickEvent.bind(this, tabObject.id));
+	trashcan.addEventListener('click', removeThisTab.bind(this, tabObject.id));
 	tab.addEventListener('click', highlightTab.bind(this, tabObject.index, tabObject.windowId));
 	return tab;
 }
 
 
+/**
+* Set new number in badge in icon
+*@param {integer} number 
+*/
 function setBadge(number) {
 	port.postMessage({ type: 'setBadge', number: number });
 }
 
-function clickEvent(id, event) {
+/**
+* Removes tab from dom
+*@param {integer} number 
+*@param {object} event
+*/
+function removeThisTab(id, event) {
 	chrome.tabs.remove(id);
 	var elem = document.querySelector('.id' + id);
 	elem.parentNode.removeChild(elem);
@@ -109,41 +118,57 @@ function clickEvent(id, event) {
 	setBadge(inactiveTabCount);
 }
 
+/**
+* Send message to extension to get pop up info
+*/
 function sendMessageToGetTabInfo() {
 	port.postMessage({ type: 'popup' });
 }
 
+/**
+* Hide login buttons when used logs in 
+*/
 function hideLoginButtons() {
     document.getElementById("logout").style.display = "block";
     document.getElementById("login").style.display = "none";
 }
 
+/**
+* removes all tabs in dom and sends message to extension to get new tab info
+*/
 function refreshContent() {
 	document.getElementById('tag-titles').innerHTML = '';
 	inactiveTabCount = 0;
 	sendMessageToGetTabInfo();
 }
 
-//try adding window id
+
+/**
+* window will focus the tab that was clicked
+*@param {integer} index
+*@param {integer} windowId
+*@param {object} event
+*/
 function highlightTab(index, windowId, event) {
 	chrome.tabs.highlight({ tabs: index, windowId: windowId });
 	chrome.windows.update(windowId, { focused: true });
 }
 
+/**
+* calls extension to log out user an removes logout btn
+*/
 function logoutUser() {
 	port.postMessage({ type: 'logout' });
-	document.getElementById('webpage').style.display = 'none';
 	document.getElementById('logout').style.display = 'none';
 	document.getElementById('login').style.display = 'block';
-	document.getElementById('signup-button').style.display = 'flex';
 }
 
-// function loginUser() {
-// 	port.postMessage({ type: 'login' });
-// }
 
 init();
 
+
+//this is a solution to a Mac issue with extension. Macs animate the extension open, so not having a set width can result in the window not having enough height to show the content
+//found this solution at https://bugs.chromium.org/p/chromium/issues/detail?id=428044 
 document.body.style.opacity = 0;
 document.body.style.transition = 'opacity ease-out .4s';
 
