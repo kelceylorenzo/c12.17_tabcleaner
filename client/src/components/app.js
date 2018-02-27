5a13226d2d2a89609bd2e4ad97f7446d7bdb90cf
Nick Quan <nickkquan@gmail.com>
2018-02-26 19:48:28 -0800

    added functionality for landing-scroll

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import axios from "axios";
 axios.defaults.withCredentials = true;
 
-import LandingPage from './landing-page';
-import MainPage from './main-page';
-import AboutPage from './about-page';
-import TopTenPage from './top-ten-page';
-import headerData from './header-data.js';
+import LandingPage from "./landing-page";
+import MainPage from "./main-page";
+import AboutPage from "./about-page";
+import TopTenPage from "./top-ten-page";
+import headerData from "./header-data.js";
 
-import '../assets/css/app.css';
+import "../assets/css/app.css";
 
 class App extends Component {
+	handleWheelScroll(event) {
+		console.log("scrollbar event captured");
+
+	}
+
 	render() {
 		return (
 			<div className="app">
 				<Switch>
-					<Route exact path="/" component={LandingPage} />
+					<Route exact path="/" render={() => <LandingPage scroll={this.handleWheelScroll} />} />
 					<Route path="/dashboard" component={MainPage} />
 					<Route path="/about" component={AboutPage} />
 					<Route path="/top-ten" component={TopTenPage} />
