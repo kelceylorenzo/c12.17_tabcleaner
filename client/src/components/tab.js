a1d7ca3cf6dd5627af28f615ef4d853181e7cd1f
m13kelore <kelceylorenzo@gmail.com>
2018-02-28 11:41:27 -0800

    added cut off for title length and made font sizes responsive

--- client/src/components/tab.js
+++ client/src/components/tab.js
@@ -13,6 +13,8 @@ export default (props) => {
 		backgroundColor: ''
 	};
 
+	let { tabTitle } = props.item;
+
 	let viewClass = {
 		container: 'tab-container',
 		tab: 'tab',
@@ -30,11 +32,15 @@ export default (props) => {
 	let inactiveElapsedTime = props.item.currentTime - props.item.deactivatedTime;
 
 	if (inactiveElapsedTime < 10000) {
-		tabStyle.backgroundColor = ''; 
+		tabStyle.backgroundColor = '';
 	} else if (inactiveElapsedTime < 25000) {
 		tabStyle.backgroundColor = 'rgba(215, 213, 170, 0.5)';
 	} else {
 		tabStyle.backgroundColor = 'rgba(156, 95, 88, 0.5)';
+	}
+
+	if (tabTitle.length > 50) {
+		tabTitle = tabTitle.slice(0, 50) + '...';
 	}
 
 	if (props.viewChange === 'list') {
@@ -58,7 +64,7 @@ export default (props) => {
 			</div>
 
 			<div className={viewClass.tab} style={tabStyle}>
-				<div className={viewClass.title}>{props.item.tabTitle}</div>
+				<div className={viewClass.title}>{tabTitle}</div>
 				<div className={viewClass.tabScreen}>
 					<img src={placeholderImage} alt="" />
 				</div>
