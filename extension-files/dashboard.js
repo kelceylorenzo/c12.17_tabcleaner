989642bb718e63ee90926ca8340447d2e590ae0f
Andrea <andrea.wayte@gmail.com>
2018-02-27 20:24:51 -0800

    deletes all tabs except any tabs with the title close your tabs

--- extension-files/dashboard.js
+++ extension-files/dashboard.js
@@ -3,23 +3,46 @@ const COOKIE_NAME =  'connect.sid';
 console.log('STAAAAAAAAB');
 
 function init(){
-    checkUserLoginStatus();
     var logoutBtn = document.getElementById('log-out-button');
+    var deleteBtn = document.getElementsByClassName('sidebar-delete');
+    checkUserLoginStatus();
     logoutBtn.addEventListener('click', logoutUser);
-    setTimeout(function(){addClickHandlersToTabs()}, 500)
+    deleteBtn[0].addEventListener('click', removeSelectedTabs);
+    setTimeout(function(){addClickHandlersToTabs()}, 800)
 }
 
 
 function addClickHandlersToTabs(){
     var closeBtn = document.getElementsByClassName("close-favicon");
     for(var index = 0; index < closeBtn.length ; index++ ){
-        console.log(closeBtn[index])
-        closeBtn[index].addEventListener('click', removeElement)
+        closeBtn[index].addEventListener('click', removeSingleTab)
     }
 }
 
-function removeElement(event){
+function removeSingleTab(event){
     var parent = event.target.closest('.tab-container');
+    removeElement(parent);
+}
+
+function removeSelectedTabs(){
+    var tabContainers = document.getElementsByClassName('tab-container');
+    for(var tab = 0; tab < tabContainers.length; tab++){
+
+        var parent = tabContainers[tab];
+        var descendents = parent.childNodes;
+        var title = descendents[1].childNodes[0].innerText;
+        let domain = (title).match(/close your tabs/gi)
+        if(!domain){
+            var classes = tabContainers[tab].className.split(" ");
+            if(classes.indexOf('tab-selected') !== -1){
+                removeElement(tabContainers[tab]);
+            }
+        }
+    }
+}
+
+function removeElement(parent){
+    console.log(parent)
     var window = parent.getAttribute('data-windowid');
     var index = parent.getAttribute('data-tabindex');
     var tabInfo = {};
@@ -30,13 +53,6 @@ function removeElement(event){
             parent.style.display = 'none';
         } 
     });
-}
-
-
-function deleteAllTabs(){
-    //get all tab-container
-    //loop through all tabs, looking for class selected
-    //send message to backgroud page to delete if selected
 }
 
 function logoutUser(){
